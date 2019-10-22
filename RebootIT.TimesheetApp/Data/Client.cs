4d09837e7723f7b87cce947e6b32403a0dfe7ca3
JEHAD HASAN <Q5317144@live.tees.ac.uk>
2019-10-22 15:12:23 +0100

    Add project files.

---
+++ RebootIT.TimesheetApp/Data/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebootIT.TimesheetApp.Data
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Company Name")]
+        public string CompanyName { get; set; }
+        [Required]
+        public string BillingAddress { get; set; }
+        [Required]
+        public string ContactName { get; set; }
+        [Required]
+        public string ContactTelephone { get; set; }
+        [Required]
+        public string ContactEmail { get; set; }
+        // hygghg
+    }
+}
