7bb54ee144fe9fff03d2539bcc6bc94cef0b5986
JEHAD HASAN <Q5317144@live.tees.ac.uk>
2019-10-24 20:21:14 +0100

    I have add Time sheet to all staff

--- RebootIT.TimesheetApp/Data/TimesheetDbContext.cs
+++ RebootIT.TimesheetApp/Data/TimesheetDbContext.cs
@@ -26,12 +26,16 @@ namespace RebootIT.TimesheetApp.Data
                 new Staff { Id = 1, Surname = "Davison", Forename = "Tyrone", Email = "tyrone.davison@example.com"},
                 new Staff { Id = 2, Surname = "Khan", Forename = "Zafar", Email = "zafar.khan@example.com" },
                 new Staff { Id = 3, Surname = "Fairbairn", Forename = "James", Email = "james.fairbairn@example.com" }
+             
+            
             );
 
             builder.Entity<Client>().HasData(
                 new Client { Id = 1, BillingAddress = "3 Hopeless Lane, Fiddler District, Middlesbrough", CompanyName = "Fiddler Fingers", ContactName = "Free Willy", ContactTelephone = "01234 121212", ContactEmail = "fw@example.com" },
                 new Client { Id = 2, BillingAddress = "42 Cloud Lane, Cloud District, Middlesbrough", CompanyName = "Pie in the Sky", ContactName = "Sally Pie", ContactTelephone = "01234 341245", ContactEmail = "sally@example.com" }
+               
             );
+
 
             builder.Entity<Location>().HasData(
                 new Location { Id = 1, Name = "Willy's Hovel", Address = "4 Wonky Way, Middlesbrough" },
@@ -39,11 +43,13 @@ namespace RebootIT.TimesheetApp.Data
                 new Location { Id = 3, Name = "Fiddler Fingers Warehouse", Address = "13 Stack It Road, Storage District, Middlesbrough" },
                 new Location { Id = 4, Name = "Sally's Place", Address = "1 Crust Avenue, Busy District, Middlesbrough" },
                 new Location { Id = 5, Name = "Sally's Takeout", Address = "3 Crust Avenue, Busy District, Middlesbrough" }
+               
             );
 
             builder.Entity<Timesheet>().HasData(
                 new Timesheet { Id = 1, MinutesWorked = 80, StaffId = 1, ClientId = 2, LocationId = 4 }
-            );
+              
+            ); 
         }
     }
 }
